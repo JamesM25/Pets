a51a72997ba478f65d0680c23661b104dd36ae43
James <motherwell.james@student.greenriver.edu>
2023-05-11 12:01:23 -0400

    Set up fat-free framework

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * James Motherwell
+ * 5/11/2024
+ * 328/pets/index.php
+ */
+
+// Turn on error reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+require_once "vendor/autoload.php";
+
+// Create an F3 (Fat-Free Framework) object
+$f3 = Base::instance();
+
+$f3->route('GET /', function () {
+    echo '<h1>Pets</h1>';
+});
+
+// Run Fat-Free
+$f3->run();
